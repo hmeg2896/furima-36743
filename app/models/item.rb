632d206f1a5f945c9f1c700ddcc64fd088f084e2
hmeg2896 <hiroyuki.asai.407@gmail.com>
2022-02-25 20:42:13 +0900

    商品出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+
+  validates :product, presence: true
+  validates :product_description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :postage_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :price, presence: true, numericality: { less_than_or_equal_to: 9999999, 
+    greater_than_or_equal_to: 300 }
+  validates :image, presence: true
+
   belongs_to :user
   has_many :comments
   has_one :order
