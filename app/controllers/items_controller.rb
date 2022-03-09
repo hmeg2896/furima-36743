5644dbfc46910a44a311b42ae3711ebbfb73fd7e
hmeg2896 <hiroyuki.asai.407@gmail.com>
2022-03-09 18:14:08 +0900

    レビューに対する修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :select_item, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show ]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
   before_action :move_to_index, only: [:edit, :update]
   
   def index
